bc154e4aa73925ee1108c9745a5b7d1f2e0a1b1a
Charles Durst <hello@charlesdurst.fr>
2025-04-10 12:49:52 +0000

    Add missing attribute

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ window.addEventListener('load', () => {
         const dateEl = child.querySelector('.event-date-field');
     
         if (dateEl) {
-            console.log('?', dateEl);
             const date = new Date(dateEl.textContent);
             const isPast  = date < new Date();
     
             const pastEl = document.createElement('div');
 
             pastEl.classList.add('item-is-past');
-            pastEl.textContent = isPast ? 'past' : 'future';
+            pastEl.setAttribute('fs-cmsfilter-field', "isPast");
+            pastEl.textContent = isPast ? 'archive' : 'à venir';
 
             child.appendChild(pastEl);
         }
